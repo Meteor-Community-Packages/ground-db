97c3f135e639e1c17189a83b9b90472667a4d2b0
Morten Henriksen <mh@gi2.dk>
2013-07-26 11:04:32 +0200

    Bump version 0.0.12

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-  summary: "\u001b[32mv0.0.11\n"+
+  summary: "\u001b[32mv0.0.12\n"+
   "\u001b[33m-----------------------------------------\n"+
   "\u001b[0m GroundDB is a thin layer providing       \n"+
   "\u001b[0m Meteor offline database and methods      \n"+
