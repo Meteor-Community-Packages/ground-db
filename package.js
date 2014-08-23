778c5f4574b063cc8a001d2ef28b1db71e073e07
Lalo Martins <lalo.martins@gmail.com>
2014-08-23 19:24:19 +0200

    0.9/unipackage support

--- package.js
+++ package.js
@@ -1,12 +1,24 @@
 Package.describe({
-  summary: "Ground Meteor.Collections offline"
+  summary: "Ground Meteor.Collections offline",
+  version: "0.0.23",
+  name: "raix:grounddb",
+  githubUrl: "https://github.com/GroundMeteor/Meteor-GroundDB/"
 });
 
 Package.on_use(function (api) {
   "use strict";
-  api.export && api.export('GroundDB');
-  api.export && api.export('_gDB', ['client', 'server'], {testOnly: true});
-  api.use(['meteor', 'underscore', 'random', 'minimongo', 'ejson', 'ejson-minimax'],
+  if(api.export !== undefined) {
+    api.export('GroundDB');
+    api.export('_gDB', ['client', 'server'], {testOnly: true});
+  }
+
+  if(api.versionsFrom !== undefined) { // 0.9+
+    api.use(['raix:ejson-minimax'], ['client', 'server']);
+  } else {
+    api.use(['ejson-minimax'], ['client', 'server']);
+  }
+
+  api.use(['meteor', 'underscore', 'random', 'minimongo', 'ejson'],
           ['client', 'server']);
 
   api.use('standard-app-packages', ['client', 'server']);
