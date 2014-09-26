0f8e0f2d28cb61fe968e66c7cd31b87681a49070
Morten Henriksen <mh@gi2.dk>
2014-09-26 10:23:39 +0200

    bump versions

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "ground:db",
-  version: "0.0.6",
+  version: "0.0.7",
   summary: "Ground Meteor.Collections offline",
   git: "https://github.com/GroundMeteor/db.git"
 });
@@ -20,7 +20,7 @@ Package.on_use(function (api) {
       'ground:util@0.0.1',
       'ground:servertime@0.0.0',
       'ground:minimax@0.0.2',
-      'ground:localstorage@0.0.0',
+      'ground:localstorage@0.0.1',
       'raix:eventemitter@0.0.2'
     ], ['client', 'server']);
 
