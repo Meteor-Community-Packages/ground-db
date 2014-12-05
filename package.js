17de59b9a247e78e24fc626a583c70fedcf371fe
Morten Henriksen <mh@gi2.dk>
2014-12-05 18:13:07 +0100

    Bump versions

--- package.js
+++ package.js
@@ -6,7 +6,7 @@ Package.describe({
 });
 
 Package.on_use(function (api) {
-  api.versionsFrom('0.9.1');
+  api.versionsFrom('1.0');
 
   api.use('meteor-platform', ['client', 'server']);
 
@@ -15,7 +15,7 @@ Package.on_use(function (api) {
     'underscore',
     'minimongo',
     'ejson',
-    'ground:util@0.0.2',
+    'ground:util@0.1.0',
     'ground:servertime@0.0.0',
     'ground:minimax@0.0.2',
     'ground:localstorage@0.0.2',
@@ -30,7 +30,7 @@ Package.on_use(function (api) {
 
   api.export('Ground');
 
-  api.use(['deps'], 'client');
+  api.use(['tracker'], 'client');
 
 
   //api.use([], 'server');
@@ -48,7 +48,7 @@ Package.on_test(function (api) {
   api.use('ground:db', ['client']);
   api.use('test-helpers', 'client');
   api.use(['tinytest', 'underscore', 'ejson', 'ordered-dict',
-           'random', 'deps']);
+           'random', 'tracker']);
 
   api.add_files('groundDB.client.tests.js', 'client');
   api.add_files('groundDB.server.tests.js', 'server');
