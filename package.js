d136dc9ddcd69f24ba5a0fd89f2e0e5e0a9334b9
Morten Henriksen <mh@gi2.dk>
2014-12-05 13:24:49 +0100

    update version and only support >1.0

--- package.js
+++ package.js
@@ -1,62 +1,37 @@
 Package.describe({
   name: "ground:db",
-  version: "0.0.9",
+  version: "0.1.0",
   summary: "Ground Meteor.Collections offline",
   git: "https://github.com/GroundMeteor/db.git"
 });
 
 Package.on_use(function (api) {
-  if (api.versionsFrom) {
+  api.versionsFrom('0.9.1');
 
-    api.versionsFrom('METEOR@0.9.1');
+  api.use('meteor-platform', ['client', 'server']);
 
-    api.use('meteor-platform', ['client', 'server']);
+  api.use([
+    'meteor',
+    'underscore',
+    'minimongo',
+    'ejson',
+    'ground:util@0.0.2',
+    'ground:servertime@0.0.0',
+    'ground:minimax@0.0.2',
+    'ground:localstorage@0.0.2',
+    'raix:eventemitter@0.0.2',
+    'raix:stubfence@1.0.0-rc2'
+  ], ['client', 'server']);
 
-    api.use([
-      'meteor',
-      'underscore',
-      'minimongo',
-      'ejson',
-      'ground:util@0.0.2',
-      'ground:servertime@0.0.0',
-      'ground:minimax@0.0.2',
-      'ground:localstorage@0.0.2',
-      'raix:eventemitter@0.0.2'
-    ], ['client', 'server']);
+  // Make sure any storage adapters are loaded first
+  // api.use([
+  //   'ground:localstorage'
+  // ], 'client', { weak: true });
 
-    // Make sure any storage adapters are loaded first
-    // api.use([
-    //   'ground:localstorage'
-    // ], 'client', { weak: true });
+  api.export('Ground');
 
-    api.export('Ground');
+  api.use(['deps'], 'client');
 
-    api.use(['deps'], 'client');
-
-  } else {
-    api.use('standard-app-packages', ['client', 'server']);
-
-    api.use([
-      'meteor',
-      'underscore',
-      'minimongo',
-      'ejson',
-      'ground-util',
-      'ground-servertime',
-      'ejson-minimax',
-      'ground-localstorage',
-      'eventemitter'
-    ], ['client', 'server']);
-
-    // Make sure any storage adapters are loaded first
-    // api.use([
-    //   'ground-localstorage'
-    // ], 'client', { weak: true });
-
-    api.export('Ground');
-
-    api.use(['deps'], 'client');
-  }
 
   //api.use([], 'server');
   //api.use(['localstorage', 'ejson'], 'client');
@@ -70,11 +45,7 @@ Package.on_use(function (api) {
 });
 
 Package.on_test(function (api) {
-  if (api.versionsFrom) {
-    api.use('ground:db', ['client']);
-  } else {
-    api.use('grounddb', ['client']);
-  }
+  api.use('ground:db', ['client']);
   api.use('test-helpers', 'client');
   api.use(['tinytest', 'underscore', 'ejson', 'ordered-dict',
            'random', 'deps']);
