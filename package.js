7f65f8a65cba678b58b7ccf44a61de0b182222b5
Morten Henriksen <mh@gi2.dk>
2014-12-21 22:41:37 +0100

    Bump minor

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "ground:db",
-  version: "0.2.6",
+  version: "0.3.0",
   summary: "Ground Meteor.Collections offline",
   git: "https://github.com/GroundMeteor/db.git"
 });
