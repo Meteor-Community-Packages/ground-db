9c9d16fb94ce7f6570d12c6f05d067e096d28e81
Morten Henriksen <mh@gi2.dk>
2015-08-25 09:31:27 +0200

    ground:db@1.0.0-alpha.3
    minor fixes

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "ground:db",
-  version: "1.0.0-alpha.2",
+  version: "1.0.0-alpha.3",
   summary: "Ground Meteor.Collections offline",
   git: "https://github.com/GroundMeteor/db.git"
 });
