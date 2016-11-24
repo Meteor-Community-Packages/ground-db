dece3b99dc2c32829558e66c16c0122f29c4ff09
Morten Henriksen <mh@gi2.dk>
2016-11-24 19:41:25 +0100

    bump version 2.0.0-rc.7

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "ground:db",
-  version: "2.0.0-rc.6",
+  version: "2.0.0-rc.7",
   summary: "Ground Meteor.Collections offline",
   git: "https://github.com/GroundMeteor/db.git"
 });
